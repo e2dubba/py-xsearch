5da8dd90ae00fb261f0c80868cfdc2b8fa5d4a34
e2dubba <svirgilgoodwin@gmail.com>
2021-01-18 06:36:31 -0700

    updating linting files and gitignore

--- xsearch/xsearch.py
+++ xsearch/xsearch.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""
+Script for using xpath to search xml files and directories to generate a command line
+report.
+"""
 
 import os
 import argparse
 from collections import OrderedDict
-from typing import List, Mapping, Dict, Tuple
+from typing import List, Mapping, Dict, Tuple, Optional
 import lxml  # type: ignore
 from lxml import etree  # type: ignore
 
@@ -11,8 +15,8 @@ from lxml import etree  # type: ignore
 def find_elements(
     file_path,
     filter_xpath='//[.="Data"]',
-    namespaces=None,
-    element_list=None,
+    namespaces=Optional[Dict[str, str]],
+    element_list=Optional[List[lxml.etree._Element]],
     **kwargs,
 ) -> Tuple[List[lxml.etree._Element], Dict[str, str]]:
     """
@@ -23,15 +27,13 @@ def find_elements(
     text = `//marc:controlfield[@tag="001" and .="ControlNumber"]`
     """
     if namespaces is None:
-        namespaces: Dict[str, str] = dict()
+        namespaces: Dict[str, str] = dict()  # type: ignore
     if element_list is None:
-        element_list: List[lxml.etree._Element] = list()
+        element_list: List[lxml.etree._Element] = list()  # type: ignore
     with open(file_path, "r") as filep:
         root = etree.parse(filep).getroot()
         namespaces.update(root.nsmap)
-        element_list += root.xpath(
-            filter_xpath, namespaces=namespaces, **kwargs
-        )
+        element_list += root.xpath(filter_xpath, namespaces=namespaces, **kwargs)
     return element_list, namespaces
 
 
@@ -52,10 +54,7 @@ def path_walker(
                 continue
             file_path = os.path.join(root, filename)
             element_list, namespaces = find_elements(
-                file_path,
-                namespaces=namespaces,
-                element_list=element_list,
-                **kwargs,
+                file_path, namespaces=namespaces, element_list=element_list, **kwargs,
             )
 
     return element_list, namespaces
@@ -93,9 +92,7 @@ def get_elements_tags(
         if parent is None:
             continue
         if args.id:
-            idn = "".join(
-                p.text for p in parent.xpath(args.id, namespaces=namespaces)
-            )
+            idn = "".join(p.text for p in parent.xpath(args.id, namespaces=namespaces))
         for xpath in results_xpath:
             result_dict = OrderedDict()
             elements = parent.xpath(xpath, namespaces=namespaces)
@@ -124,7 +121,6 @@ def nice_rows(rows: List[List[str]], pad=3):
     Also, make sure all of the data is passed as strings.
     """
     col_widths = []
-    # for i in range(len(rows[0])):
     for i in range(len(rows[0])):
         col_widths.append(max(len(str(row[i])) for row in rows))
 
@@ -139,17 +135,17 @@ def main():
     Read through a directory to find elements and their relationship to other files
     """
     aparse = argparse.ArgumentParser(
-        description="Read through a directory \
-            of xml files and return different elements and values of the xml"
+        description=(
+            "Read through a directory             of xml files and return different"
+            " elements and values of the xml"
+        )
     )
     aparse.add_argument(
         "-d",
         "--dir",
         help="Specify the starting directory for recursively transversing",
     )
-    aparse.add_argument(
-        "-i", "--id", help="Set an Xpath to the parent id number"
-    )
+    aparse.add_argument("-i", "--id", help="Set an Xpath to the parent id number")
     aparse.add_argument(
         "-a",
         "--attrib",
@@ -160,28 +156,21 @@ def main():
         "-x", "--text", action="store_true", help="Return the tag text value"
     )
     aparse.add_argument(
-        "-l",
-        "--tail",
-        action="store_true",
-        help="Return the element tail text",
+        "-l", "--tail", action="store_true", help="Return the element tail text",
     )
     aparse.add_argument(
         "-g", "--tag", action="store_true", help="Return the Element tag"
     )
-    aparse.add_argument(
-        "--padding", help="Set padding for the column rows", type=int
-    )
-    aparse.add_argument(
-        "-p", "--parent", help="Parent tag to return parent element"
-    )
-    aparse.add_argument(
-        "filter_xpath", help="The xpath query to filter the records on"
-    )
+    aparse.add_argument("--padding", help="Set padding for the column rows", type=int)
+    aparse.add_argument("-p", "--parent", help="Parent tag to return parent element")
+    aparse.add_argument("filter_xpath", help="The xpath query to filter the records on")
     aparse.add_argument(
         "results_xpath",
         nargs="*",
-        help="Find a set of values from the parent of the filter record, \
-                if not set, will just print the parent record",
+        help=(
+            "Find a set of values from the parent of the filter record,                "
+            " if not set, will just print the parent record"
+        ),
     )
     args = aparse.parse_args()
 
@@ -190,29 +179,23 @@ def main():
     else:
         directory = args.dir
 
-    element_list, namespaces = path_walker(
-        directory, filter_xpath=args.filter_xpath
-    )
+    element_list, namespaces = path_walker(directory, filter_xpath=args.filter_xpath)
     if args.parent:
         nmp, tag = args.parent.split(":", maxsplit=1)
         expanded = namespaces[nmp]
         parent_tag = "{%s}%s" % (expanded, tag)
     else:
         parent_tag = args.parent
-    parent_generator = map(
-        lambda x: get_parent_by_tag(x, parent_tag), element_list
-    )
-    results = get_elements_tags(
-        parent_generator, args, args.results_xpath, namespaces
-    )
+    parent_generator = map(lambda x: get_parent_by_tag(x, parent_tag), element_list)
+    results = get_elements_tags(parent_generator, args, args.results_xpath, namespaces)
     if args.padding:
         padding = args.padding
     else:
         padding = 3
     if len(results) != 0:
         rows: List[str] = [list(results[0].keys())]
-        for r in results:
-            rows.append([x.replace("\n", "\\n") for x in r.values()])
+        for result in results:
+            rows.append([x.replace("\n", "\\n") for x in result.values()])
         nice_rows(rows, padding)
 
 
